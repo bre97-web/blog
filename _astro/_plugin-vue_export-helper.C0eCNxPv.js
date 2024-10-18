bb5684bc79b23210342aa09a9f13536ed3fb6ab5
bre97-web <bre97-web@users.noreply.github.com>
2024-10-18 13:53:16 +0000

    deploy: 285ee0e945b747388b28cf6d0a62b128a2b0f24a

---
+++ _astro/_plugin-vue_export-helper.C0eCNxPv.js
@@ -0,0 +1,1 @@
+import{s as a,l as c,m as n}from"./runtime-core.esm-bundler.BiIBErin.js";function l(e){let s=a(),t=e.subscribe(o=>{s.value=o});return c()&&n(t),s}const p=(e,s)=>{const t=e.__vccOpts||e;for(const[o,r]of s)t[o]=r;return t};export{p as _,l as u};

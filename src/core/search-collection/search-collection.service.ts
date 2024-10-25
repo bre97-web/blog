652f9c623c876b92e9532c874cc3e33513f7391a
bre97-web <bre97.web@gmail.com>
2024-10-25 17:29:36 +0800

    chore: upload mdx, change tsconfig

--- src/core/search-collection/search-collection.service.ts
+++ src/core/search-collection/search-collection.service.ts
@@ -1,24 +1,24 @@
-import { atom } from "nanostores";
-import type { TBlog } from "src/content/config";
+import type { CollectionEntry } from 'astro:content'
+import { atom } from "nanostores"
 
 export class SearchCollectionService {
     public static readonly inputValue = atom('')
 
-    public static findPostByBody(searchText: string, collection: Array<TBlog>) {
-        return collection.filter(e => e.body.includes(searchText));
+    public static findPostByBody(searchText: string, collection: Array<CollectionEntry<'blogs'>>) {
+        return collection.filter(e => e.body.includes(searchText))
     }
 
-    public static findPostByFileName(fileName: string, collection: Array<TBlog>) {
-        return collection.filter(e => e.id.includes(fileName));
+    public static findPostByFileName(fileName: string, collection: Array<CollectionEntry<'blogs'>>) {
+        return collection.filter(e => e.id.includes(fileName))
     }
 
-    public static findPostByCollectionName(collectionName: string, collection: Array<TBlog>) {
-        return collection.filter(e => e.data.collection.includes(collectionName));
+    public static findPostByCollectionName(collectionName: string, collection: Array<CollectionEntry<'blogs'>>) {
+        return collection.filter(e => e.data.collection.includes(collectionName))
     }
-    public static findPostByTitle(searchText: string, collection: Array<TBlog>) {
-        return collection.filter(e => e.data.title.includes(searchText));
+    public static findPostByTitle(searchText: string, collection: Array<CollectionEntry<'blogs'>>) {
+        return collection.filter(e => e.data.headline.includes(searchText))
     }
-    public static findPostByDescription(searchText: string, collection: Array<TBlog>) {
-        return collection.filter(e => e.data.description.includes(searchText));
+    public static findPostByDescription(searchText: string, collection: Array<CollectionEntry<'blogs'>>) {
+        return collection.filter(e => e.data.description.includes(searchText))
     }
 }
